210028a010e7262f543cebf5df81cde2d430af8e
Gikuyu Nderitu <gikuyunderitu@gmail.com>
2016-10-22 12:59:50 -0400

    Add comments and begin post function

--- server.js
+++ server.js
@@ -2,32 +2,21 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
+let todoNextId = 1;
 let todos = [
-  {
-    id: 1,
-    description: 'Meet mom for lunch',
-    completed: false
-  },
-  {
-    id: 2,
-    description: 'Make wife "happy"',
-    completed: false
-  },
-  {
-    id: 3,
-    description: 'Find out how to make camera work from vlc player',
-    completed: true
-  }
+
 ]
 
 app.get('/', (req, res)=>{
   res.send('Todo API Root')
 })
 
+//GET /todos
 app.get('/todos', (req, res)=>{
   res.json(todos)
 })
 
+//GET /todos/:id
 app.get('/todos/:id', (req,res)=>{
   let todo = todos.find(val =>{
     return val.id === parseInt(req.params.id, 10)
@@ -37,6 +26,11 @@ app.get('/todos/:id', (req,res)=>{
     res.status(404).send()
   else
     res.json(todo)
+})
+
+// POST /todos
+app.post('/todos',(req, res)=>{
+
 })
 
 app.listen(port, ()=>{
