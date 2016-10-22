173ba19584602d64fb02c2ad2cc88021f5028600
Gikuyu Nderitu <gikuyunderitu@gmail.com>
2016-10-22 12:43:33 -0400

    Add todos and todos id

--- server.js
+++ server.js
@@ -2,9 +2,41 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
+let todos = [
+  {
+    id: 1,
+    description: 'Meet mom for lunch',
+    completed: false
+  },
+  {
+    id: 2,
+    description: 'Make wife "happy"',
+    completed: false
+  },
+  {
+    id: 3,
+    description: 'Find out how to make camera work from vlc player',
+    completed: true
+  }
+]
 
 app.get('/', (req, res)=>{
   res.send('Todo API Root')
+})
+
+app.get('/todos', (req, res)=>{
+  res.json(todos)
+})
+
+app.get('/todos/:id', (req,res)=>{
+  let todo = todos.find(val =>{
+    return val.id === parseInt(req.params.id, 10)
+  })
+
+  if(typeof todo == 'undefined')
+    res.status(404).send()
+  else
+    res.json(todo)
 })
 
 app.listen(port, ()=>{
